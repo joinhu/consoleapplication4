3945dc8b03d7f93131b87398a5aeb9a1bfd86f4d
joinhu <karthikmadamshetty222@gmail.com>
2015-09-24 13:00:10 +0530

    upload new source code
    consoleapplication

---
+++ MY PROGRAM 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedListNode<string> node = new
+        LinkedListNode<string>("KARTHIK");
+        LinkedList<string> names = new LinkedList<string>();
+        names.AddFirst(node);
+        LinkedListNode<string> node1 = new
+        LinkedListNode<string>
+        ("ANIL");
+        names.AddAfter(node, node1);
+        LinkedListNode<string> node2 = new
+        LinkedListNode<string>
+        ("SUNIL");
+        names.AddAfter(node1, node2);
+        LinkedListNode<string> node3 = new LinkedListNode
+        <string>(null);
+        LinkedListNode<string> aNode = names.First;
+        while (aNode != null)
+        {
+            Console.WriteLine(aNode.Value);
+            aNode = aNode.Next;
+        }
+        aNode = names.Find("SUMAN");
+        if (aNode != null) aNode = names.First;
+        while (aNode != null)
+        {
+            Console.WriteLine(aNode.Value);
+            aNode = aNode.Next;
+        }
+        Console.Read();
+    }
+}
